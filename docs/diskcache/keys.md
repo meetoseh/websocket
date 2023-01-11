e1b2cff185043a2810d1c66594163d1815ec1250
Timothy Moore <mtimothy984@gmail.com>
2023-01-11 11:38:26 -0800

    one-at-a-time updating

--- docs/diskcache/keys.md
+++ docs/diskcache/keys.md
@@ -2,15 +2,19 @@
 
 the keys we store locally on websocket instances via diskcache.
 
--   `journeys:{uid}:meta`: meta information about the journey with the given uid.
-    [used here](../../journeys/lib/meta.py)
+- `journeys:{uid}:meta`: meta information about the journey with the given uid.
+  [used here](../../journeys/lib/meta.py)
 
-    ```json
-    {
-        "uid": "string",
-        "duration_seconds": 0
-    }
-    ```
+  ```json
+  {
+    "uid": "string",
+    "duration_seconds": 0
+  }
+  ```
 
-    where `uid` is just the uid of the journey, `duration_seconds` is the duration
-    of the journeys audio content
+  where `uid` is just the uid of the journey, `duration_seconds` is the duration
+  of the journeys audio content
+
+- `updater-lock-key` goes to a random token for the token we used to acquire
+  the updater lock before shutting down to update. See the redis key
+  `updates:{repo}:lock` for more information.
--- updater.py
+++ updater.py
@@ -1,17 +1,72 @@
 """Handles updating when the repository is updated"""
+from itgs import Itgs
 import perpetual_pub_sub as pps
+import asyncio
 import subprocess
 import platform
+import secrets
 import os
 
 
-async def listen_forever():
+async def _listen_forever():
     """Subscribes to the redis channel updates:websocket and upon
     recieving a message, calls /home/ec2-user/update_webapp.sh
     """
+    async with Itgs() as itgs:
+        await release_update_lock_if_held(itgs)
+
     async with pps.PPSSubscription(pps.instance, "updates:websocket", "updater") as sub:
-        async for _ in sub:
+        await sub.read()
+
+    async with Itgs() as itgs:
+        await acquire_update_lock(itgs)
+
+    do_update()
+
+
+async def acquire_update_lock(itgs: Itgs):
+    our_identifier = secrets.token_urlsafe(16).encode("utf-8")
+    local_cache = await itgs.local_cache()
+
+    redis = await itgs.redis()
+    while True:
+        local_cache.set(b"updater-lock-key", our_identifier, expire=310)
+        success = await redis.set(
+            b"updates:websocket:lock", our_identifier, nx=True, ex=300
+        )
+        if success:
             break
+        await asyncio.sleep(1)
+
+
+DELETE_IF_MATCH_SCRIPT = """
+local key = KEYS[1]
+local expected = ARGV[1]
+
+local current = redis.call("GET", key)
+if current == expected then
+    redis.call("DEL", key)
+    return 1
+end
+return 0
+"""
+
+
+async def release_update_lock_if_held(itgs: Itgs):
+    local_cache = await itgs.local_cache()
+
+    our_identifier = local_cache.get(b"updater-lock-key")
+    if our_identifier is None:
+        return
+
+    redis = await itgs.redis()
+    await redis.eval(
+        DELETE_IF_MATCH_SCRIPT, 1, b"updates:websocket:lock", our_identifier
+    )
+    local_cache.delete(b"updater-lock-key")
+
+
+def do_update():
     if platform.platform().lower().startswith("linux"):
         subprocess.Popen(
             "bash /home/ec2-user/update_webapp.sh > /dev/null 2>&1",
@@ -27,3 +82,30 @@ async def listen_forever():
             creationflags=subprocess.CREATE_NEW_PROCESS_GROUP,
             close_fds=True,
         )
+
+
+async def listen_forever():
+    """Subscribes to the redis channel updates:websocket and upon
+    recieving a message, calls /home/ec2-user/update_webapp.sh
+    """
+    if os.path.exists("updater.lock"):
+        return
+    with open("updater.lock", "w") as f:
+        f.write(str(os.getpid()))
+
+    try:
+        await _listen_forever()
+    finally:
+        os.unlink("updater.lock")
+        print("updater shutdown")
+
+
+def listen_forever_sync():
+    """Subscribes to the redis channel updates:websocket and upon
+    recieving a message, calls /home/ec2-user/update_webapp.sh
+    """
+    asyncio.run(listen_forever())
+
+
+if __name__ == "__main__":
+    listen_forever_sync()
